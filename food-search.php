2f1deca8c9670cc98102aa3ceca87bcfc615a880
Rhvdes <110344944+Rhvdes@users.noreply.github.com>
2023-07-12 04:02:07 +0100

    worked on css page mostly the food-menu-box

--- food-search.php
+++ food-search.php
@@ -1,92 +1,86 @@
-<?php 
-        include('partials-front/menu.php');
+<?php
+include('partials-front/menu.php');
 ?>
 
-    <!-- fOOD sEARCH Section Starts Here -->
-    <section class="food-search text-center">
-        <div class="container">
-            <?php
-                  //get the search keywords
-                  $search=mysqli_real_escape_string($conn, $_POST['search']);
+<!-- fOOD sEARCH Section Starts Here -->
+<section class="food-search text-center">
+    <div class="container">
+        <?php
+        // Check if the search key is set in $_POST array
+        if (isset($_POST['search'])) 
+        {
+            //get the search keywords
+            $search = mysqli_real_escape_string($conn, $_POST['search']);
             ?>
-            <h2>Foods on Your Search <a href="#" class="text-white"><?php echo $search;?></a></h2>
-
-        </div>
-    </section>
-    <!-- fOOD sEARCH Section Ends Here -->
-
-
-
-    <!-- fOOD MEnu Section Starts Here -->
-    <section class="food-menu">
-        <div class="container">
-            <h2 class="text-center">Food Menu</h2>
-
+            <h2>Foods on Your Search <a href="#" class="text-white"><?php echo $search; ?></a></h2>
             <?php
-              //create sql query to get food based on search keyword
-                $sql="SELECT * FROM tbl_food WHERE title LIKE '%$search%' OR description LIKE '%$search%'";
-                //execute the query
-                $result=mysqli_query($conn, $sql);
-                //count rows
-                $count=mysqli_num_rows($result);
-                //check whether food is available or not
-                if($count>0)
+            //create sql query to get food based on search keyword
+            $sql = "SELECT * FROM tbl_food WHERE title LIKE '%$search%' OR description LIKE '%$search%'";
+            //execute the query
+            $result = mysqli_query($conn, $sql);
+            //count rows
+            $count = mysqli_num_rows($result);
+            //check whether food is available or not
+            if ($count > 0) 
+            {
+                //food is available
+                //use while loop to display all available food
+                while ($row = mysqli_fetch_assoc($result)) 
                 {
-                    //food is available
-                    //use while loop to display all available food
-                    while($row=mysqli_fetch_assoc($result))
-                    {
-                        $id=$row['id'];
-                        $title=$row['title'];
-                        $price=$row['price'];
-                        $description=$row['description'];
-                        $image_name=$row['image_name'];
-                        ?>
-                        <div class="food-menu-box">
-                            <div class="food-menu-img">
-                            <?php 
-                                //check if image is available or not
-                                if($image_name=="")
-                                {
-                                    //image is not available
-                                    echo "<div class='error'>Image not Found</div>";
-                                }
-                                else
-                                {
-                                    //image is available
-                                    ?>
-                                    <img src="<?php echo SITEURL;?>images/food/<?php echo $image_name;?>" alt="Chicke Hawain Pizza" class="img-responsive img-curve">
-                                    <?php
-                                }
+                    $id = $row['id'];
+                    $title = $row['title'];
+                    $price = $row['price'];
+                    $description = $row['description'];
+                    $image_name = $row['image_name'];
+                    ?>
+                    <div class="food-menu-box">
+                        <div class="food-menu-img">
+                            <?php
+                            //check if image is available or not
+                            if ($image_name == "") 
+                            {
+                                //image is not available
+                                echo "<div class='error'>Image not Found</div>";
+                            } 
+                            else 
+                            {
+                                //image is available
+                                ?>
+                                <img src="<?php echo SITEURL; ?>images/food/<?php echo $image_name; ?>"
+                                     alt="" class="img-responsive img-curve">
+                                <?php
+                            }
                             ?>
-                            
-                            </div>
-                            <div class="food-menu-desc">
-                            <h4><?php echo $title;?></h4>
-                            <p class="food-price">&#8358;<?php echo $price;?></p>
-                            <p class="food-detail"><?php echo $description;?></p>
+
+                        </div>
+                        <div class="food-menu-desc">
+                            <h4><?php echo $title; ?></h4>
+                            <p class="food-price">&#8358;<?php echo $price; ?></p>
+                            <p class="food-detail"><?php echo $description; ?></p>
                             <br>
                             <a href="#" class="btn btn-primary">Order Now</a>
-                            </div>
                         </div>
-                        <?php
-                    }
+                    </div>
+                    <?php
                 }
-                else
-                {
-                    //food is not available
-                    echo "<div class='error'>Food is not Available</div>";
-                }
-            ?>
-            <div class="clearfix"></div>
+            } 
+            else 
+            {
+                //food is not available
+                echo "<div class='error'>Food is not Available</div>";
+            }
+        } 
+        else 
+        {
+            // Search key is not set in $_POST array
+            echo "<h2 class='text-white'>Please perform a search first.</h2>";
+        }
+        ?>
+        <div class="clearfix"></div>
+    </div>
+</section>
+<!-- fOOD Menu Section Ends Here -->
 
-            
-
-        </div>
-
-    </section>
-    <!-- fOOD Menu Section Ends Here -->
-    <?php 
-        include('partials-front/footer.php');
+<?php
+include('partials-front/footer.php');
 ?>
-   
--- order.php
+++ order.php
@@ -82,16 +82,16 @@
                 <fieldset>
                     <legend>Delivery Details</legend>
                     <div class="order-label">Full Name</div>
-                    <input type="text" name="full-name" placeholder="E.g. Vijay Thapa" class="input-responsive" required>
+                    <input type="text" name="full-name" placeholder="E.g. Sulaiman Awwal" class="input-responsive" required>
 
                     <div class="order-label">Phone Number</div>
-                    <input type="tel" name="contact" placeholder="E.g. 9843xxxxxx" class="input-responsive" required>
+                    <input type="tel" name="contact" placeholder="E.g. 070xxxxxx" class="input-responsive" required>
 
                     <div class="order-label">Email</div>
-                    <input type="email" name="email" placeholder="E.g. hi@vijaythapa.com" class="input-responsive" required>
+                    <input type="email" name="email" placeholder="E.g. johndoe@gmail.com" class="input-responsive" required>
 
                     <div class="order-label">Address</div>
-                    <textarea name="address" rows="10" placeholder="E.g. Street, City, Country" class="input-responsive" required></textarea>
+                    <textarea name="address" rows="10" class="input-responsive" required></textarea>
 
                     <input type="submit" name="submit" value="Confirm Order" class="btn btn-primary">
                 </fieldset>
@@ -120,7 +120,7 @@
                     price=$price,
                     qty=$qty,
                     total=$total,
-                    order_date='$order_date',
+                    order_date=NOW(),
                     status='$status',
                     customer_name='$customer_name',
                     customer_contact='$customer_contact',
